39a87c776c15755dacaf692cfb4ec7b388174df7
Manoj <shahmanojkumar975@gmail.com>
2024-12-05 22:04:52 +0530

    insertion and selection sort added in the class

--- java_dsa/sorting/sortdef.java
+++ java_dsa/sorting/sortdef.java
@@ -14,7 +14,36 @@ class sortdef {
         }
     }
 
+    public void selectSort(int []arr){
+        int n= arr.length;
+        int i=0,j=0,min=-1, temp=0;
+        
+        for(i=0;i<n-1;i++){
+            min=i;
+            for(j=i+1;j<n;j++){
+                if(arr[j]<arr[min])
+                min=j;
+            }
+            temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
 
+        }
+    }
+
+    public void insertsort(int []arr){
+        int n=arr.length;
+        int i=1,j=0,key=0;
+        for(;i<n;i++){
+            key=arr[i];
+            j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
 
 public void display(int[] arr){
     for(int i=0;i<arr.length;i++){
